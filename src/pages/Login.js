a92d8a7deec1cfb5fafa7c5fb47f9361eb0ab18f
Samuel Vitor <appestoquecontado@gmail.com>
2025-05-05 19:08:15 -0300

    Ajuste nas margens da caixa de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -27,8 +27,8 @@ function Login() {
   return (
     <div className="login-container">
       <h1 className="titulo-login">Login</h1>
-      {/* Aqui a classe de fundo foi modificada */}
-      <form onSubmit={handleLogin} className="bg-white sm:bg-transparent p-6 rounded-lg shadow-md">
+      {/* Caixa de login com margens maiores */}
+      <form onSubmit={handleLogin} className="bg-white p-6 rounded-lg shadow-md sm:max-w-lg sm:mx-auto sm:my-16 my-8 mx-4">
         <div className="input-group">
           <label htmlFor="email">E-mail</label>
           <input
